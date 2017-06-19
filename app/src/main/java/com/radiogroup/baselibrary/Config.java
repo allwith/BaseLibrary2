dfb1c9aa30ae58cb08a78615b54c56966a32b01d
allwith <952054711@qq.com>
2017-06-19 14:43:07 +0800

    06.19

---
+++ app/src/main/java/com/radiogroup/baselibrary/Config.java
@@ -0,0 +1,26 @@
+package com.radiogroup.baselibrary;
+
+
+import com.radiogroup.baselibrary.utils.Utils;
+
+import java.io.File;
+
+/**
+ * <pre>
+ *     author: Blankj
+ *     blog  : http://blankj.com
+ *     time  : 2017/05/10
+ *     desc  :
+ * </pre>
+ */
+public class Config {
+    public static final String PKG      = "com.blankj.androidutilcode";
+    public static final String TEST_PKG = "com.blankj.testinstall";
+    private static String testApkPath;
+
+    public static String getTestApkPath() {
+        if (testApkPath == null)
+            testApkPath = Utils.getContext().getCacheDir().getAbsolutePath() + File.separatorChar + "apk" + File.separatorChar + "test_install.apk";
+        return testApkPath;
+    }
+}
